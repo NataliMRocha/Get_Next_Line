cdf312f561ac806654ebb3655a947e4dcdbe9cea
Natali Moreira Rocha <namoreir@c3r3p2.42sp.org.br>
2023-08-22 19:54:06 -0300

    get_next_line_header

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 10:39:04 by namoreir          #+#    #+#             */
-/*   Updated: 2023/08/21 11:31:07 by namoreir         ###   ########.fr       */
+/*   Updated: 2023/08/22 19:53:46 by namoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -27,8 +26,27 @@ struct s_list{
 	t_list	*next;
 };
 
+/**@brief Creates a new node of the linked list and put a buffer
+ * character in the node.
+ * 
+ * @param c the character to be placed
+ * @return The new node
+ * */
 t_list			*get_char_node(char c);
+
+/**@brief Puts the node at the end of the linked list.
+ * If don't ecxist list, the new node will be the head of list.
+ * 
+ * @param lst the list
+ * @param new the new node
+ * */
 void			put_in_pos(t_list **lst, t_list *new);
+
+/**@brief frees the allocated memory if the read returns -1.
+ * 
+ * @param line the list to be freed
+ * @return null pointer
+ * */
 char			*free_file(t_list *line);
 
 typedef struct s_file{
@@ -40,8 +58,27 @@ typedef struct s_file{
 	t_list		*line;
 }t_file;
 
+/**@brief The principal function, defines the value of variables inside struct
+ * cleans the buffer, and makes the first read. 
+ * 
+ * @param fd the int representing a file descriptor
+ * @return a string passed as a parameter to the read_line function
+ * */
 char			*get_next_line(int fd);
-char			*read_line(t_file *file);
-char			*get_current_line(t_file *file);
+
+/**@brief Makes a loop to read the file, create new nodes with each character
+ * read, counts the size of bytes stored until it finds \n or \0
+ * 
+ * @param temp a struct with multiple variables
+ * @return a string passed as a parameter to the get_current_line function
+ * */
+char			*read_line(t_file *temp);
+
+/**@brief creates a new string and copy each character from the list into it
+ * 
+ * @param set a struct with multiple variables 
+ * @return a string with the line
+ * */
+char			*get_current_line(t_file *set);
 
 #endif
